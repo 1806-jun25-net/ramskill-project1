44a6e94bb797de59f704cc42be2bf2f555394fe6
Daniel <nephesh1@gmail.com>
2018-07-01 12:28:21 -0400

    added more menu functionality

--- PizzaStore/PizzaStore.Library/Models/Order.cs
+++ PizzaStore/PizzaStore.Library/Models/Order.cs
@@ -27,63 +27,89 @@ namespace PizzaStore.Library.Models
         {
             string menuSelect;
 
-            Console.WriteLine($"Please select an option from the menu to contine.\n1. Order a Classic Pizza\n2. Order a custom pizza\n" +
-                                $"3. View order\n4. Checkout\n5. Change user\n6. Change location\n7. Quit Application");
+            Console.WriteLine($"Please select an option from the menu to contine.\n1. Add Pizza\n2. View order\n3. Checkout\n4. Change user\n5. Change location\n6. Quit Application");
             menuSelect = Console.ReadLine();
             menuSelect = menuSelect.ToLower();
 
             switch(menuSelect)
             {
                 case "1":
-                    ChooseClassicPizza();
+                    AddPizza();
                     break;
 
                 case "2":
-                    CreateCustomPizza();
-                    break;
-
-                case "3":
                     ViewOrder();
                     break;
 
-                case "4":
+                case "3":
                     Checkout();
                     break;
 
-                case "5":
+                case "4":
                     ChangeUser();
                     break;
 
-                case "6":
+                case "5":
                     ChangeLocation();
                     break;
 
-                case "7":
+                case "6":
                     QuitApp();
                     break;
 
-                default: 
-                    
-
-
-
+                default:
+                    mainMenu();
+                    break;
             }
                  
 
             return menuSelect;
         }
 
-        public void CreateCustomPizza()
+        public void AddPizza()
+        {
+            string menuSelect = null;
+
+            Console.WriteLine("1. Add Classic Pizza\n2. Add Custom Pizza\n3. Main Menu");
+            menuSelect = Console.ReadLine();
+            menuSelect = menuSelect.ToLower();
+
+            if (menuSelect == "1" || menuSelect == "classic")
+            {
+                AddClassicPizza();
+            }
+            else if (menuSelect == "2" || menuSelect == "custom")
+            {
+                AddCustomPizza();
+            }
+            else if (menuSelect == "3" || menuSelect == "menu" || menuSelect == "main menu")
+            {
+                mainMenu();
+            }
+            else
+            {
+                Console.WriteLine("Please selct a pizza to add.\n");
+                AddPizza();
+            }
+
+        }
+
+        public void AddClassicPizza()
         {
             throw new NotImplementedException();
         }
 
-        public void ChooseClassicPizza()
-        { 
+        public void AddCustomPizza()
+        {
             throw new NotImplementedException();
         }
 
         public void ViewOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Checkout()
         {
             throw new NotImplementedException();
         }
@@ -98,14 +124,20 @@ namespace PizzaStore.Library.Models
             throw new NotImplementedException();
         }
 
-        public void Checkout()
-        {
-            throw new NotImplementedException();
-        }
-
         public void QuitApp()
         {
-            throw new NotImplementedException();
+            string quitSelect;
+            Console.WriteLine("Are you sure you want to quit? If you have not checked out, your order will be lost.");
+            quitSelect = Console.ReadLine();
+            quitSelect = quitSelect.ToLower();
+            if (quitSelect == "yes" || quitSelect == "y")
+            {
+                System.Environment.Exit(1);
+            }
+            else
+            {
+                mainMenu();
+            }
         }
 
     }
