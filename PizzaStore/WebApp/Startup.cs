aa226c8d07ca3af74b3ffe5c1cfc34c42a1863c8
Daniel Ramskill <dramskil@uncc.edu>
2018-07-12 17:01:04 -0400

    Added: view templates, controller templates, Menu item. Removed: Home page template stuff

--- PizzaStore/WebApp/Startup.cs
+++ PizzaStore/WebApp/Startup.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PizzaStore.Data;
 
 namespace WebApp
 {
@@ -27,10 +28,13 @@ namespace WebApp
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            //services.AddScoped<RestaurantRepository>();
+            services.AddDbContext<PizzaStoreDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("PizzaStoreDB")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -45,10 +49,8 @@ namespace WebApp
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
